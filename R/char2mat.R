ec48834a8670ab4a9745e236ee041bbeec25fc8a
yixuan <yixuan.qiu@cos.name>
2014-08-10 18:07:01 -0400

    matrix viewing function

--- R/char2mat.R
+++ R/char2mat.R
@@ -1,5 +1,13 @@
-char2mat = function(ch = "A")
+char2mat = function(ch = "A", dim = 50)
 {
     ch = as.character(ch)
-    .Call("char2mat", ch, PACKAGE = "asciir")
+    dim = as.integer(dim)
+    .Call("char2mat", ch, dim, PACKAGE = "asciir")
+}
+
+viewmat = function(mat)
+{
+    m = nrow(mat)
+    n = ncol(mat)
+    image(1:n, 1:m, t(mat[m:1, ]), col = grey(45:0/50), asp = 1)
 }
