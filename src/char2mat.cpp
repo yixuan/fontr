e44536a99c4ffa6eda84ee0870506f6a47d3557a
yixuan <yixuan.qiu@cos.name>
2014-08-10 18:06:22 -0400

    allow choosing pixel size

--- src/char2mat.cpp
+++ src/char2mat.cpp
@@ -6,10 +6,12 @@
 using namespace Rcpp;
 using std::string;
 
-RcppExport SEXP char2mat(SEXP _chr) {
+RcppExport SEXP char2mat(SEXP _chr, SEXP _dim)
+{
 BEGIN_RCPP
     string str = as<string>(_chr);
     char ch = str[0];
+    int pixel_size = as<int>(_dim);
     
     FT_Library    library;
     FT_Face       face;
@@ -23,22 +25,32 @@ BEGIN_RCPP
     error = FT_New_Face(library, filename, 0, &face);/* create face object */
     /* error handling omitted */
 
-    /* use 50pt at 100dpi */
-    error = FT_Set_Char_Size(face, 50 * 64, 0,
-                             100, 0 );                /* set character size */
+    /* use 50 pixels */
+    error = FT_Set_Pixel_Sizes(face, pixel_size, pixel_size);
     /* error handling omitted */
 
     slot = face->glyph;
     error = FT_Load_Char(face, ch, FT_LOAD_RENDER);
     
+    int left = slot->bitmap_left;
+    int top = pixel_size - slot->bitmap_top;
     int width = slot->bitmap.width;
-    int height = slot->bitmap.rows;
+    int rows = slot->bitmap.rows;
+    int xmax = left + width;
+    int ymax = top + rows;
+    int advance = slot->advance.x / 64;
     
-    NumericMatrix mat(width, height);
-    for(int i = 0; i < width; i++)
-        for(int j = 0; j < height; j++)
-            mat(i, height - j - 1) = slot->bitmap.buffer[i + width * j];
+    NumericMatrix mat(pixel_size, advance);
+    for(int i = top, p = 0; i < ymax; i++, p++)
+    {
+        for(int j = left, q = 0; j < xmax; j++, q++)
+        {
+            if(i < 0 || j < 0 || i >= pixel_size || j >= advance)
+                continue;
 
+            mat(i, j) = slot->bitmap.buffer[p * width + q];
+        }
+    }
     FT_Done_Face    ( face );
     FT_Done_FreeType( library );
 
