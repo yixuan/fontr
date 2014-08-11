fe4e192f755268397229f509751ffcf58102c722
yixuan <yixuan.qiu@cos.name>
2014-08-10 21:35:25 -0400

    documentation

--- src/fonts.cpp
+++ src/fonts.cpp
@@ -5,11 +5,16 @@ using std::string;
 
 FT_Face get_ft_face(string &family)
 {
+    // Get the namespace environment of package "sysfonts"
     Environment sysfonts = Environment::namespace_env("sysfonts");
+    // Get the .pkg.env environment defined in sysfonts
     Environment pkgenv = sysfonts[".pkg.env"];
+    // Get the font list
     List font_list = pkgenv[".font.list"];
+    // Family names of the registered fonts
     CharacterVector font_names = font_list.names();
     
+    // Search for font with the specified family name
     int i;
     char *font_name;
     for(i = 0; i < font_names.length(); i++)
@@ -18,9 +23,10 @@ FT_Face get_ft_face(string &family)
         if(font_name == family)
             break;
     }
+    // If not found, use the default mono family
     if(i == font_names.length())
         i = 2; // mono
-    
+    // Data stored in font list are pointers to FontDesc structure
     List font_selected = font_list[i];
     SEXP extPtr = font_selected[0];
     FontDesc *font = (FontDesc *) R_ExternalPtrAddr(extPtr);
