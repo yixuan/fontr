248210d492bf1fb9ce3e480422904a1af83cce5a
Yixuan Qiu <yixuan.qiu@cos.name>
2015-03-20 14:15:32 -0400

    image of a string

--- src/char2mat.cpp
+++ src/char2mat.cpp
@@ -7,10 +7,32 @@
 #include "fonts.h"
 
 using Rcpp::as;
-using Rcpp::NumericMatrix;
+using Rcpp::IntegerMatrix;
 using std::string;
 
-RcppExport SEXP char2mat(SEXP chr_, SEXP family_, SEXP fontface_, SEXP dim_)
+// Write bitmap to the matrix
+// The upper-left corner of the bitmap will be written to matrix(i, j)
+void write_matrix(const FT_Bitmap *bitmap, IntegerMatrix mat, int mi, int mj)
+{
+    // Bitmap is stored by row, starting from the upperleft corner
+    // Therefore, the pixel in the p-th row and q-th column (0-based) is
+    // buffer[p * bm_width + q]
+    int bm_width = bitmap->width, bm_height = bitmap->rows;
+    int mat_cols = mat.ncol(), mat_rows = mat.nrow();
+
+    for(int i = mi, p = 0; p < bm_height; i++, p++)
+    {
+        for(int j = mj, q = 0; q < bm_width; j++, q++)
+        {
+            if(i < 0 || j < 0 || i >= mat_rows || j >= mat_cols)
+                continue;
+
+            mat(i, j) = bitmap->buffer[p * bm_width + q];
+        }
+    }
+}
+
+RcppExport SEXP char2mat(SEXP chr_, SEXP family_, SEXP fontface_, SEXP pixel_size_)
 {
 BEGIN_RCPP
     string str = as<string>(chr_);
@@ -19,43 +41,33 @@ BEGIN_RCPP
     int nchar = utf8toucs4(unicode, str.c_str(), maxlen);
     string family = as<string>(family_);
     int fontface = as<int>(fontface_);
-    int pixel_size = as<int>(dim_);
+    int pixel_size = as<int>(pixel_size_);
 
     FT_Face       face = get_ft_face(family, fontface);
     FT_GlyphSlot  slot;
     FT_Error      error;
 
-    /* use 50 pixels */
+    // Set pixel size
     error = FT_Set_Pixel_Sizes(face, pixel_size, pixel_size);
-    /* error handling omitted */
+    // Error handling omitted
 
-    int max_bearingY, max_tail;
-    get_global_metrics(face, &max_bearingY, &max_tail);
+    // How many rows above baseline
+    int bearingY;
+    // How many rows below baseline
+    int tail;
+    // How many columns
+    int advance;
+    get_string_metrics(face, unicode, nchar, &bearingY, &tail, &advance);
     
-    // Determine baseline: pixels from top canvas edge to baseline
-    int baseline = pixel_size - max_tail;
-
+    int mi = 0, mj = 0;
     slot = face->glyph;
-    error = FT_Load_Char(face, unicode[0], FT_LOAD_RENDER);
-    
-    int advance = slot->advance.x / 64;
-    int left = slot->bitmap_left + (pixel_size - advance) / 2;
-    int top = baseline - slot->bitmap_top;
-    int width = slot->bitmap.width;
-    int rows = slot->bitmap.rows;
-    int xmax = left + width;
-    int ymax = top + rows;
-
-    NumericMatrix mat(pixel_size, pixel_size);
-    for(int i = top, p = 0; i < ymax; i++, p++)
+    IntegerMatrix mat(bearingY + tail, advance);
+    for(int i = 0; i < nchar; i++)
     {
-        for(int j = left, q = 0; j < xmax; j++, q++)
-        {
-            if(i < 0 || j < 0 || i >= pixel_size || j >= pixel_size)
-                continue;
-
-            mat(i, j) = slot->bitmap.buffer[p * width + q];
-        }
+        FT_Load_Char(face, unicode[i], FT_LOAD_RENDER);
+        mi = bearingY - slot->bitmap_top;
+        write_matrix(&(slot->bitmap), mat, mi, mj + slot->bitmap_left);
+        mj += slot->advance.x / 64;
     }
 
     delete [] unicode;
