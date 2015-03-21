1e1c6525f7dbcdbc42e87852c13de37ce661ded4
Yixuan Qiu <yixuan.qiu@cos.name>
2015-03-21 14:05:46 -0400

    support text rotation

--- R/char2mat.R
+++ R/char2mat.R
@@ -1,10 +1,11 @@
-char2mat = function(ch = "A", family = "mono", fontface = 1, size = 50)
+char2mat = function(ch = "A", family = "mono", fontface = 1, size = 50, rot = 0)
 {
     ch = as.character(ch)
     family = as.character(family)
     fontface = as.integer(fontface)
     size = as.integer(size)
-    .Call("char2mat", ch, family, fontface, size, PACKAGE = "fontr")
+    rot = as.numeric(rot) / 180 * pi
+    .Call("char2mat", ch, family, fontface, size, rot, PACKAGE = "fontr")
 }
 
 viewmat = function(mat)
--- src/char2mat.cpp
+++ src/char2mat.cpp
@@ -27,12 +27,15 @@ void write_matrix(const FT_Bitmap *bitmap, IntegerMatrix mat, int mi, int mj)
             if(i < 0 || j < 0 || i >= mat_rows || j >= mat_cols)
                 continue;
 
-            mat(i, j) = bitmap->buffer[p * bm_width + q];
+            unsigned char intensity = bitmap->buffer[p * bm_width + q];
+            if(intensity > 0)
+                mat(i, j) = intensity;
         }
     }
 }
 
-RcppExport SEXP char2mat(SEXP chr_, SEXP family_, SEXP fontface_, SEXP pixel_size_)
+RcppExport SEXP char2mat(SEXP chr_, SEXP family_, SEXP fontface_,
+                         SEXP pixel_size_, SEXP rot_)
 {
 BEGIN_RCPP
     string str = as<string>(chr_);
@@ -42,33 +45,46 @@ BEGIN_RCPP
     string family = as<string>(family_);
     int fontface = as<int>(fontface_);
     int pixel_size = as<int>(pixel_size_);
+    double rot = as<double>(rot_);
 
     FT_Face       face = get_ft_face(family, fontface);
-    FT_GlyphSlot  slot;
+    FT_GlyphSlot  slot = face->glyph;
     FT_Error      error;
 
     // Set pixel size
     error = FT_Set_Pixel_Sizes(face, pixel_size, pixel_size);
     // Error handling omitted
 
-    // How many rows above baseline
-    int bearingY;
-    // How many rows below baseline
-    int tail;
-    // How many columns
-    int advance;
-    get_string_metrics(face, unicode, nchar, &bearingY, &tail, &advance);
+    // Bounding box
+    int xmin, xmax, ymin, ymax;
+    get_string_bbox(face, unicode, nchar, rot, &xmin, &xmax, &ymin, &ymax);
+    
+    // Set rotation for transformation
+    FT_Matrix trans;
+    FT_Vector pen;
+    trans.xx = trans.yy = (FT_Fixed)( cos(rot) * 0x10000L);
+    trans.xy = (FT_Fixed)(-sin(rot) * 0x10000L);
+    trans.yx = -trans.xy;
+    pen.x = pen.y = 0;
     
     int mi = 0, mj = 0;
-    slot = face->glyph;
-    IntegerMatrix mat(bearingY + tail, advance);
+    IntegerMatrix mat(ymax - ymin, xmax - xmin);
     for(int i = 0; i < nchar; i++)
     {
+        FT_Set_Transform(face, &trans, &pen);
         FT_Load_Char(face, unicode[i], FT_LOAD_RENDER);
-        mi = bearingY - slot->bitmap_top;
-        write_matrix(&(slot->bitmap), mat, mi, mj + slot->bitmap_left);
-        mj += slot->advance.x / 64;
+        mi = ymax - slot->bitmap_top;
+        mj = slot->bitmap_left - xmin;
+        write_matrix(&(slot->bitmap), mat, mi, mj);
+        pen.x += slot->advance.x;
+        pen.y += slot->advance.y;
     }
+    
+    // Restore to identity
+    trans.xx = trans.yy = 0x10000L;
+    trans.xy = trans.yx = 0;
+    pen.x = pen.y = 0;
+    FT_Set_Transform(face, &trans, &pen);
 
     delete [] unicode;
     
