3bc792439d93d9d34a0012bd3dcbc7d69694c03f
Yixuan Qiu <yixuan.qiu@cos.name>
2015-09-24 22:56:47 -0400

    add plotting function

--- R/glyph_outline.R
+++ R/glyph_outline.R
@@ -12,5 +12,13 @@ glyph_polygon = function(ch = "a", family = "sans", face = 1, nseg = 10)
     family = as.character(family)
     face = as.integer(face)
     nseg = as.integer(nseg)
-    .Call("glyph_polygon", ch, family, face, nseg, PACKAGE = "fontr")
+    dat = .Call("glyph_polygon", ch, family, face, nseg, PACKAGE = "fontr")
+    class(dat) = "glyph_polygon"
+    dat
+}
+
+plot.glyph_polygon = function(glyph, ...)
+{
+    plot(1, type = "n", xlim = c(0, 1), ylim = c(0, 1), asp = 1, ...)
+    polypath(glyph$x, glyph$y)
 }
