1a6aafd53dd881ab85bc5eabe887daa445024dd8
Yixuan Qiu <yixuan.qiu@cos.name>
2015-09-24 22:29:06 -0400

    plotting function

--- R/glyph_bitmap.R
+++ R/glyph_bitmap.R
@@ -1,16 +1,19 @@
-glyph_bitmap = function(ch = "A", family = "mono", fontface = 1, size = 50, rot = 0)
+glyph_bitmap = function(ch = "A", family = "mono", face = 1, pixel_size = 50, rot = 0)
 {
     ch = as.character(ch)
     family = as.character(family)
-    fontface = as.integer(fontface)
-    size = as.integer(size)
+    face = as.integer(face)
+    pixel_size = as.integer(pixel_size)
     rot = as.numeric(rot) / 180 * pi
-    .Call("glyph_bitmap", ch, family, fontface, size, rot, PACKAGE = "fontr")
+    mat = .Call("glyph_bitmap", ch, family, face, pixel_size, rot, PACKAGE = "fontr")
+    class(mat) = "glyph_bitmap"
+    mat
 }
 
-viewmat = function(mat)
+plot.glyph_bitmap = function(glyph)
 {
-    m = nrow(mat)
-    n = ncol(mat)
-    image(1:n, 1:m, t(mat[m:1, ]), col = grey(45:0/50), asp = 1)
+    m = nrow(glyph)
+    n = ncol(glyph)
+    image(1:n, 1:m, t(glyph[m:1, ]), col = grey((45:0) / 50), asp = 1,
+          axes = FALSE, xlab = "", ylab = "")
 }
